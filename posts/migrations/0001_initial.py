59edc3d1b542fb150057bdf4c5a65239f81957e7
Rinji47 <biswasankur06@gmail.com>
2026-02-21 22:29:47 +0545

    Updated the import

--- posts/migrations/0001_initial.py
+++ posts/migrations/0001_initial.py
@@ -45,7 +45,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('slug', models.SlugField(blank=True, max_length=220, unique=True)),
-                ('content', django_quill.fields.QuillField()),
+                ('content', models.TextField()),
                 ('status', models.CharField(choices=[('draft', 'Draft'), ('published', 'Published')], default='published', max_length=20)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
